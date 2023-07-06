852d4cbb0ec3eec25b99b1c952773011e7ffc75b
ManpreetPannu25 <manpreet9421@gmail.com>
2023-07-06 13:29:52 -0400

    modified trial package

--- src/trialPackage/StudentMain.java
+++ src/trialPackage/StudentMain.java
@@ -1,14 +1,12 @@
 package trialPackage;
 
-
-
 public class StudentMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 StudentName abc = new StudentName();
-	        abc.studentName = "Guntaas Pannu";
-	abc.displayStudentName(); 
+		StudentName abc = new StudentName();
+		abc.studentName = "Guntaas Pannu";
+		abc.displayStudentName();
 
-}
+	}
 }
