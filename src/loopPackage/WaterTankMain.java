0e95897f97f38b5c14f1736d8ec914deca8c081a
ManpreetPannu25 <manpreet9421@gmail.com>
2023-07-12 12:27:17 -0400

    Revised water tank Main

--- src/loopPackage/WaterTankMain.java
+++ src/loopPackage/WaterTankMain.java
@@ -4,9 +4,6 @@ public class WaterTankMain {
 
 	public static void main(String[] args) {
 		WaterTank capacity = new WaterTank();
-
-		capacity.waterLevel = 0;
-		capacity.tankFinalCapacity = 100;
 		capacity.tankFillingMechanism();
 
 	}
