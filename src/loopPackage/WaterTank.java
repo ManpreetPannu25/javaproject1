306671be818853150a23a9093133688014eb4628
ManpreetPannu25 <manpreet9421@gmail.com>
2023-07-12 14:13:59 -0400

    Water Tank revised1

--- src/loopPackage/WaterTank.java
+++ src/loopPackage/WaterTank.java
@@ -2,9 +2,9 @@ package loopPackage;
 
 public class WaterTank {
 	int tankTotalCapacity = 100;
-	double waterLevel = 15;
+	double waterLevel = 67;
 	int bucketCapacity = 10;
-	double tankMaxCapacity = 91;// assumed max water capacity for the tank to avoid overflow
+	double tankMaxCapacity = 95;// assumed max water capacity for the tank to avoid overflow
 	double requiredWater; // (water required at the end to fill the tank at its exact Max capacity,despite
 							// bucket capacity)
 
@@ -16,11 +16,14 @@ public class WaterTank {
 						+ " litres water on top of it,to fill the tank at its max water Level");
 				break;
 			} else if (waterLevel == tankMaxCapacity) {
-				System.out.println("Water tank is already at its max capacity,avoid overflow...");
-
+				System.out.println(
+						"Water tank is already at its max capacity of " + waterLevel + " litres, avoid overflow...");
+				break;
 			}
+
 			System.out.println("Tank water level is " + waterLevel + " litres ");
 			waterLevel += bucketCapacity;
 		}
+
 	}
 }
