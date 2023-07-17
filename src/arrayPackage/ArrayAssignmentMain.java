c39d99bc0929c1cae30150cf72e46ca3b6b580db
ManpreetPannu25 <manpreet9421@gmail.com>
2023-07-17 13:18:12 -0400

    revised array main assignment

--- src/arrayPackage/ArrayAssignmentMain.java
+++ src/arrayPackage/ArrayAssignmentMain.java
@@ -6,7 +6,14 @@ public class ArrayAssignmentMain {
 
 	public static void main(String[] args) {
 		ArrayAssignment phones = new ArrayAssignment();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(
+				"Please type the Brand name(Samsung/Google/Apple) to see the available products in that category");
+		phones.enteredBrandName = sc.next();
 		phones.findingProductName();
-		phones.verifyEnteredProducts();
+
+		phones.verifySelectedProduct();
+
 	}
 }
