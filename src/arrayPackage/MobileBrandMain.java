5df9f04699b462295d6346404a01d953bb69cb51
ManpreetPannu25 <manpreet9421@gmail.com>
2023-07-18 12:44:51 -0400

    Revised MobileBrand Main Assignment

--- src/arrayPackage/MobileBrandMain.java
+++ src/arrayPackage/MobileBrandMain.java
@@ -2,18 +2,16 @@ package arrayPackage;
 
 import java.util.Scanner;
 
-public class ArrayAssignmentMain {
+public class MobileBrandMain {
 
 	public static void main(String[] args) {
-		ArrayAssignment phones = new ArrayAssignment();
+		MobileBrandAssignment phones = new MobileBrandAssignment();
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println(
 				"Please type the Brand name(Samsung/Google/Apple) to see the available products in that category");
 		phones.enteredBrandName = sc.next();
-		phones.findingProductName();
-
-		phones.verifySelectedProduct();
+		phones.verifySelectedBrandAndItsProducts();
 
 	}
 }
